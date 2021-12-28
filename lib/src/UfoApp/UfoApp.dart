9120c36444aa8ca850997fc61b23b92b5d5b7d91
supermouse2496 <supermouse2496+supermouse2496@noreply.github.com>
2021-12-28 21:23:07 +0900

    UfoApp,UfoAppStateを実装

---
+++ lib/src/UfoApp/UfoApp.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class UfoApp extends StatefulWidget{
+  @override
+  State createState() => UfoAppState();
+}
+
+class UfoAppState extends State<UfoApp>{
+  @override
+  Widget build(BuildContext ctx) {
+    return MaterialApp(
+        title: '打越祭オンライン',
+        theme: ThemeData(
+            primarySwatch: Colors.blue
+        )
+    );
+  }
+}
